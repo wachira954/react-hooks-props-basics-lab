d402fc484c77aa5f655ddbf92f5c84b25909acf0
wachira <wachiraa954@gmail.com>
2022-07-15 15:50:29 +0300

    commit tree 3dcc31651fdacc97732113dff256329bbba271d7

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,9 @@ function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <links github={props.links.github} linkedin={props.links.linkein} />
     </div>
   );
 }
