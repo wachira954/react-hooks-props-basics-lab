9d4fef436cf33a457e1fb61fe43aae2a753596f6
wachira <wachiraa954@gmail.com>
2022-07-16 10:52:53 +0300

    aboutjs props assgn done

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,9 @@ function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      { props.bio && props.bio.length > <p>{props.bio}</p> }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <links github={ props.links.github } linkedin={props.links.linkedin} />
     </div>
   );
 }
